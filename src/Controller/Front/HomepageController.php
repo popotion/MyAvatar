984b846b0ccbacaa4acce309ae02585d5f6459a7
Romain Millan <romain@romainmillan.fr>
2024-01-11 18:31:47 +0100

    ✨ Homepage

--- src/Controller/Front/HomepageController.php
+++ src/Controller/Front/HomepageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Front;
 
+use App\Form\SeeMyAvatarHomepageFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,12 +14,30 @@ use Symfony\Component\Routing\Attribute\Route;
     requirements: [
         '_locale' => 'en|fr',
     ],
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
 )]
 class HomepageController extends AbstractController
 {
-    public function __invoke(): Response
-    {
-        return $this->render('front/homepage.html.twig');
+    public function __invoke(
+        Request $request,
+    ): Response {
+        $form = $this->createForm(SeeMyAvatarHomepageFormType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            dd($data);
+
+            // return $this->redirectToRoute(
+            //     RouteCollection::SEE_MY_AVATAR->value,
+            //     [
+            //         'username' => $data['username'],
+            //     ]
+            // );
+        }
+
+        return $this->render('front/homepage.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
